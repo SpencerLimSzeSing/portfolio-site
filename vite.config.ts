720a3fc6c68e407104ddf25b86c80784eb764b75
SpencerLimSzeSing <szesinglim@gmail.com>
2026-01-20 23:45:20 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const REPO_NAME = 'portfolio-site';
-
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production'
-    ? `/${REPO_NAME}/`
-    : '/',
-})
+  base: "portfolio-site",
+});
